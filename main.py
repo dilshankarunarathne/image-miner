9f715bdf5b3006e23a475f7c4e2391f6ee78323d
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-04 11:37:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import time
-from selenium import webdriver, By
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
