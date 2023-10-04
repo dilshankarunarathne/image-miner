ddbb14cc1d0fb76015dbe7b779541595cabe9ac9
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-04 11:53:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ downloader.download(
     limit=100,  
     output_dir='dataset', 
     adult_filter_off=True, 
-    force_replace=False, timeout=60, verbose=True)
+    force_replace=False, 
+    timeout=60, verbose=True)
