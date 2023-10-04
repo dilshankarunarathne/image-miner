8f3525a6fc42e4eb9cddb3e55d840f0011c27846
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-04 11:42:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 
 def download_images(search_query, num_images):
