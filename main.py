12139692e51271c1cb0e43e600affedeb5ecce52
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-04 11:53:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ downloader.download(
     "dog", 
     limit=100,  
     output_dir='dataset', 
-    adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
+    adult_filter_off=True, 
+    force_replace=False, timeout=60, verbose=True)
