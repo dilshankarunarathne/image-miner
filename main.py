ec17bf434dd22aac24f7752e65ff9fb2cc07e5bd
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-04 11:47:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,93 @@
+import argparse
+import json
+import itertools
+import logging
+import re
 import os
-import time
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
+import uuid
+import sys
+from urllib.request import urlopen, Request
+
+from bs4 import BeautifulSoup
 
 
-def download_images(search_query, num_images):
-    # Create a new folder for the search query if it doesn't exist
-    if not os.path.exists(search_query):
-        os.makedirs(search_query)
+def configure_logging():
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler()
+    handler.setFormatter(
+        logging.Formatter('[%(asctime)s %(levelname)s %(module)s]: %(message)s'))
+    logger.addHandler(handler)
+    Filehandler = logging.FileHandler("G:\log.txt") #Path to your LOG FILE.
+    Filehandler.setFormatter(
+        logging.Formatter('[%(asctime)s %(levelname)s %(module)s]: %(message)s'))
+    logger.addHandler(Filehandler)
+    
+    return logger
 
-    # Set up Chrome WebDriver
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Run Chrome in headless mode (no GUI)
-    driver = webdriver.Chrome(options=chrome_options)
+logger = configure_logging()
 
-    try:
-        # Perform a Google image search
-        search_url = f"https://www.google.com/search?q={search_query}&tbm=isch"
-        driver.get(search_url)
-
-        # Scroll down to load more images (you might need to adjust this based on the number of images you want)
-        for _ in range(num_images // 20):
-            driver.find_element('body').send_keys(Keys.END)
-            time.sleep(2)  # Wait for 2 seconds to load more images
-
-        # Get the URLs of the top images
-        image_urls = []
-        images = driver.find_element("css selector", ".rg_i")
-        for i, image in enumerate(images):
-            if i >= num_images:
-                break
-            image.click()
-            time.sleep(1)
-            image_url = driver.find_element("css selector", ".n3VNCb").get_attribute("src")
-            image_urls.append(image_url)
-
-        # Download the images
-        for i, image_url in enumerate(image_urls):
-            image_data = requests.get(image_url).content
-            with open(f"{search_query}/{i}.jpg", "wb") as f:
-                f.write(image_data)
-
-    except Exception as e:
-        print(f"Error: {e}")
-        print(f"Current URL: {driver.current_url}")
-
-    finally:
-        # Quit the WebDriver
-        driver.quit()
+REQUEST_HEADER = {
+    'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
 
 
-download_images("cats", 10)
+def get_soup(url, header):
+    response = urlopen(Request(url, headers=header))
+    return BeautifulSoup(response, 'html.parser')
+
+def get_query_url(query):
+    return "https://www.google.co.in/search?q=%s&source=lnms&tbm=isch" % query
+
+def extract_images_from_soup(soup):
+    image_elements = soup.find_all("div", {"class": "rg_meta"})
+    metadata_dicts = (json.loads(e.text) for e in image_elements)
+    link_type_records = ((d["ou"], d["ity"]) for d in metadata_dicts)
+    return link_type_records
+
+def extract_images(query, num_images):
+    url = get_query_url(query)
+    logger.info("Souping")
+    soup = get_soup(url, REQUEST_HEADER)
+    logger.info("Extracting image urls")
+    link_type_records = extract_images_from_soup(soup)
+    return itertools.islice(link_type_records, num_images)
+
+def get_raw_image(url):
+    req = Request(url, headers=REQUEST_HEADER)
+    resp = urlopen(req)
+    return resp.read()
+
+def save_image(raw_image, image_type, save_directory):
+    extension = image_type if image_type else 'jpg'
+    file_name = uuid.uuid4().hex + "." + extension
+    save_path = os.path.join(save_directory, file_name)
+    with open(save_path, 'wb') as image_file:
+        image_file.write(raw_image)
+
+def download_images_to_dir(images, save_directory, num_images):
+    for i, (url, image_type) in enumerate(images):
+        try:
+            logger.info("Making request (%d/%d): %s", i, num_images, url)
+            raw_image = get_raw_image(url)
+            save_image(raw_image, image_type, save_directory)
+        except Exception as e:
+            logger.exception(e)
+
+def run(query, save_directory, num_images=100):
+    query = '+'.join(query.split())
+    logger.info("Extracting image links")
+    images = extract_images(query, num_images)
+    logger.info("Downloading images")
+    download_images_to_dir(images, save_directory, num_images)
+    logger.info("Finished")
+
+def main():
+    parser = argparse.ArgumentParser(description='Scrape Google images')
+    parser.add_argument('-s', '--search', default='bananas', type=str, help='search term')
+    parser.add_argument('-n', '--num_images', default=100, type=int, help='num images to save')
+    parser.add_argument('-d', '--directory', default='G:\__main__\tf_files\flower_photos', type=str, help='save directory')
+    args = parser.parse_args()
+    run(args.search, args.directory, args.num_images)
+
+if __name__ == '__main__':
+    main()
